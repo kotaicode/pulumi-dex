0b5976d766c22779cf862e3690bd9c16588b86d8
Andreas Leicher <email@andreasleicher.com>
2026-02-14 22:20:07 +0100

    fix(ci): set version already locally, do not run npm version again

--- pkg/provider/version.go
+++ pkg/provider/version.go
@@ -2,4 +2,4 @@ package provider
 
 // Version is the version of the provider. It is set at build time using ldflags.
 // Default value is used for local development.
-var Version = "0.7.3"
+var Version = "0.7.4"
