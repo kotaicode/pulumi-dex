07262e1da4d1fea972307b09005ad00aed43009b
Andreas Leicher <email@andreasleicher.com>
2026-02-19 20:43:46 +0100

    feat(release): release v0.7.12

--- sdk/python/python/setup.py
+++ sdk/python/python/setup.py
@@ -9,7 +9,7 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
-VERSION = "0.7.11"
+VERSION = "0.7.12"
 def readme():
     try:
         with open('README.md', encoding='utf-8') as f:
